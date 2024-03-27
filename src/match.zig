af1c4bd4d75c5f25943faaf7d0783fbcdcd8a543
Dennis Schulze <dennis.benedikt.schulze@proton.me>
2024-03-27 16:51:14 +0100

    extended matching to return type and function parameters

--- src/match.zig
+++ src/match.zig
@@ -17,9 +17,58 @@ pub fn match(alloc: std.mem.Allocator, target: defs.FunctionDef, candidats: []de
     return metrics;
 }
 
-fn matchMetric(target: defs.FunctionDef, candidat: defs.FunctionDef) i32 {
-    if (std.mem.containsAtLeast(u8, candidat.name, 1, target.name)) {
-        return 10;
+fn matchLoosely(target: []const u8, candidat: []const u8) i32 {
+    var current: usize = 0;
+    var matchvalue: i32 = 0;
+    for (candidat) |c| {
+        if (current >= target.len) {
+            break;
+        }
+        if (c == target[current]) {
+            current += 1;
+            matchvalue += 3;
+        }
     }
-    return 0;
+    matchvalue -= @as(i32, @intCast((target.len - current) * 2));
+    return matchvalue;
+}
+
+fn matchLooselyReversed(target: []const u8, candidat: []const u8) i32 {
+    var currentTarget: usize = 0;
+    var currentCandidat: usize = 0;
+    var matchvalue: i32 = 0;
+    while (currentCandidat < candidat.len and currentTarget < target.len) {
+        const charTarget = target[target.len - currentTarget - 1];
+        const charCandidat = candidat[candidat.len - currentCandidat - 1];
+        if (charTarget == charCandidat) {
+            currentTarget += 1;
+            matchvalue += 3;
+        }
+        currentCandidat += 1;
+    }
+    matchvalue -= @as(i32, @intCast((candidat.len - currentTarget) * 2));
+    return matchvalue;
+}
+
+fn matchMetric(target: defs.FunctionDef, candidat: defs.FunctionDef) i32 {
+    const matchName = matchLoosely(target.name, candidat.name);
+    const matchReturn = matchLooselyReversed(target.return_type, candidat.return_type);
+    const diff: i128 = @as(i128, target.params.items.len) - @as(i128, candidat.params.items.len);
+    var matchParams = if (std.math.absInt(diff)) |d| @as(i32, @truncate(d)) * -20 else |_| 0;
+    if (diff == 0) {
+        for (target.params.items, candidat.params.items) |t, c| {
+            matchParams += matchLooselyReversed(t, c);
+        }
+    } else if (diff > 0) {
+        var i: usize = 0;
+        while (i < candidat.params.items.len) : (i += 1) {
+            matchParams += matchLooselyReversed(target.params.items[i], candidat.params.items[i]);
+        }
+    } else {
+        var i: usize = 0;
+        while (i < target.params.items.len) : (i += 1) {
+            matchParams += matchLooselyReversed(target.params.items[i], candidat.params.items[i]);
+        }
+    }
+    return matchName + matchParams + matchReturn;
 }
