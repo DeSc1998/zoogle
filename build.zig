9a571b549c56c7a976898e8ea9ab47dd9ab96c52
Dennis Schulze <dennis.benedikt.schulze@proton.me>
2024-04-05 23:14:42 +0200

    added test case back

--- build.zig
+++ build.zig
@@ -35,4 +35,14 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    const exe_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_tests = b.addRunArtifact(exe_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_tests.step);
 }
